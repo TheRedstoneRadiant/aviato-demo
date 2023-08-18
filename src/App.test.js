e1e059aed8da40c82ae96a0b5b009ff0ce9cfe09
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-08-18 16:03:24 -0400

    Create tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders preloader", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const preloaderElement = screen.getByTestId("preloader");
+  expect(preloaderElement).toBeInTheDocument();
+});
+
+test("renders main content when loading is complete", () => {
+  render(<App isLoading={false} />);
+  const navigationBarElement = screen.getByRole("navigation");
+  expect(navigationBarElement).toBeInTheDocument();
+});
+
+test("renders navigation bar", () => {
+  render(<App isLoading={false} />);
+  const navigationBarElement = screen.getByRole("navigation");
+  expect(navigationBarElement).toBeInTheDocument();
+});
+
+test("renders images", () => {
+  render(<App isLoading={false} />);
+  const demoImage = screen.getByAltText("Demo");
+  const investorsImage = screen.getByAltText("Investors");
+  expect(demoImage).toBeInTheDocument();
+  expect(investorsImage).toBeInTheDocument();
+});
+
+test("renders investor text", () => {
+  render(<App isLoading={false} />);
+  const investorText = screen.getByText(/Investors already using Aviato/i);
+  expect(investorText).toBeInTheDocument();
 });
--- src/components/preloader.component.js
+++ src/components/preloader.component.js
@@ -9,7 +9,7 @@ export default function Preloader({ setIsLoading }) {
   }, [setIsLoading]);
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center opacity-100 transition-opacity duration-500">
+    <div className="w-screen h-screen flex items-center justify-center opacity-100 transition-opacity duration-500" data-testid="preloader">
       <div className="animate-spin rounded-full border-t-4 border-blue-500 border-opacity-50 h-16 w-16"></div>
     </div>
   );
