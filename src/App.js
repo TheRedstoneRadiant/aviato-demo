61521947b05d95b94e0ae4f1b4e5d01b7507aa6c
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-08-18 15:37:01 -0400

    Create Preloader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import React, { useState, useEffect } from "react";
 import NavigationBar from "./components/navigationBar.component";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setIsLoading(false);
+    });
+
+    return () => {
+      window.removeEventListener("load", () => {
+        setIsLoading(false);
+      });
+    };
+  }, []);
+
   return (
     <div className="m-0 font-inter">
+      {isLoading && (
+        <div className="w-screen h-screen flex items-center justify-center opacity-100 transition-opacity duration-500">
+          <div className="animate-spin rounded-full border-t-4 border-blue-500 border-opacity-50 h-16 w-16"></div>
+        </div>
+      )}
+
       <div
-        className="w-screen h-screen overflow-x-hidden overflow-y-scroll flex flex-col items-center"
+        className={`w-screen h-screen overflow-x-hidden overflow-y-scroll flex flex-col items-center ${
+          isLoading
+            ? "opacity-0"
+            : "opacity-100 transition-opacity duration-500"
+        }`}
         style={{
           background:
             "radial-gradient(123.96% 100% at 50.00% -0.00%, rgba(57, 129, 246, 0.15) 35.50%, rgba(255, 255, 255, 0.00) 68.24%, rgba(57, 129, 246, 0.00) 100%)",
