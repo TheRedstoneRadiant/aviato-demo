3cc5ce50c3c11b05f42cefe3fd606d8d70bf5186
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-08-18 23:56:16 -0400

    Navbar links

--- src/components/navigationBar.component.js
+++ src/components/navigationBar.component.js
@@ -5,7 +5,9 @@ export default function NavigationBar() {
   return (
     <nav className="mt-6 flex justify-center">
       <div className="flex items-center justify-around rounded-2xl bg-white p-3 text-xs font-semibold drop-shadow-md sm:text-sm">
-        <AviatoLogo />
+        <a href="/" target="_blank">
+          <AviatoLogo />
+        </a>
         <div className="flex space-x-4 px-4 text-gray-950 sm:space-x-8 sm:px-8">
           <a href="/" target="_blank">
             Team
@@ -17,11 +19,17 @@ export default function NavigationBar() {
         <div className="space-x-1.5">
           <a
             href="https://www.aviato.co/funds/auth/login"
+            rel="noreferrer"
             className="px-2 py-2 font-semibold text-primary-500 sm:px-3.5"
+            target="_blank"
           >
             Sign In
           </a>
-          <a href="https://www.aviato.co/sales">
+          <a
+            href="https://www.aviato.co/sales"
+            target="_blank"
+            rel="noreferrer"
+          >
             <button className="rounded-lg bg-primary-100 px-2 py-2 text-white sm:px-3.5 sm:py-2">
               Request a free trial
             </button>
